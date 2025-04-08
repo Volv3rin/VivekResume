843436b5390012922bb48e31cafd4f226d785cb7
Vivek Nayee <volverin.aka.volvf@gmail.com>
2025-04-08 13:56:34 +0530

    Added my profile Details
    Trying upload

---
+++ data/vivek_profileData.js
@@ -0,0 +1,118 @@
+const profileData = {
+  "title": "Resume",
+  "name": "Vivek H Nayee",
+  "sub_title": "IT Infrastructure Manager",
+  "logoURL": "assets/images/dp.jpg",
+  "about": {
+    "intro": "As an IT Professional I am having more than 12 years of experience in IT Infrastructure Design and Implementation and have worked on various projects in multiple domains which includes IT Software Monitoring, Healthcare, IT services, Telecom sector.",
+    "contact": {
+      "email": "viveknayee@outlook.com",
+      "phone": "+91-9727216292",
+      "address": "Navi Mumbai, IN"
+    }
+  },
+  "links": [
+    {
+      "title": "LinkedIn",
+      "src": ""
+    }
+  ],
+  "education": [
+    {
+      "alma": "G.H Patel College of Engineering, Sardar Patel University",
+      "duration": "2006 - 2010",
+      "std": "B.E. (Information Technology)",
+      "score": "5.4 CGPA"
+    },
+    {
+      "alma": "St Mary\u2019s School, GHSEB Board, Rajkot",
+      "duration": "2004 - 2006",
+      "std": "Class XI - XII (Science)",
+      "score": "65.40%"
+    },
+    {
+      "alma": "St Mary\u2019s School, GHSEB Board, Rajkot",
+      "duration": "1992 - 2004",
+      "std": "Class X (General)",
+      "score": "77.57%"
+    }
+  ],
+  "skills": [
+    {
+      "category": "Skills",
+      "topics": [
+        "RHEL",
+        "Ubuntu",
+        "OpenBSD",
+        "FreeBSD",
+        "Mandriva",
+        "CentOS",
+        "DebianOS",
+        "Solaris 11",
+        "SCO Unixware",
+        "MacOSx Server",
+        "Windows Server \u2013 2003 2008/R2",
+        "VmwareESXi 4&5",
+        "IBM - AIX"
+      ]
+    }
+  ],
+  "projects": [
+    {
+      "title": "Restaurant Review",
+      "duration": "2018-06 - 2018-11",
+      "link": "https://github.com/imvpn22/restaurant-review-pwa",
+      "desc": "A web-app to list, view and add reviews of restaurents. The app includes filtering based on location, cuisine and rating. Developed using HTML5, CSS3 and JavaScript with PWA features including responsiveness, offline support and accessibility."
+    },
+    {
+      "title": "WhiteBoard",
+      "duration": "2017-06 - 2017-08",
+      "link": "https://github.com/imvpn22/whiteboard",
+      "desc": "A simple web-app for collaborative brainstorming sessions. Real-time messaging and sketching platform using Node.js, Express, EJS, CSS, JavaScript, and Socket.io."
+    }
+  ],
+  "experiences": [
+    {
+      "organization": "Jio Platforms Ltd",
+      "title": "Manager - IT Infrastructure Support",
+      "date": "2015-10 - ",
+      "details": [
+        ""
+      ]
+    },
+    {
+      "organization": "Jio Platforms Ltd",
+      "title": "Deputy Manager - IT Infrastructure Support",
+      "date": "2015-10 - 2018-12",
+      "details": [
+        ""
+      ]
+    },
+    {
+      "organization": "IGATE Global Solution Ltd",
+      "title": "Senior Systems Engineer",
+      "date": "2014-10 - 2015-09",
+      "details": [
+        ""
+      ]
+    },
+    {
+      "organization": "eClinicalWorks India Pvt Ltd",
+      "title": "Systems Administrator",
+      "date": "2013-05 - 2014-09",
+      "details": [
+        ""
+      ]
+    },
+    {
+      "organization": "MindArray Systems Pvt Ltd",
+      "title": "System Administrator",
+      "date": "2011-05 - 2013-02",
+      "details": [
+        ""
+      ]
+    }
+  ],
+  "certifications": [],
+  "events": []
+};
